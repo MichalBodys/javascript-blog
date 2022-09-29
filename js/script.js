4f8f110eb47358d671da86ea95910a50997b1c6a
Michał <bodysmic@gmail.com>
2022-09-29 14:29:31 +0200

    find correct article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function titleClickHandler(event) {
+	event.preventDefault();
 	const clickedElement = this;
 	console.log("Link was clicked!");
 
@@ -23,9 +24,12 @@ function titleClickHandler(event) {
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove("active");
 	}
-	/* get 'href' attribute from the clicked link */
-
-	/* find the correct article using the selector (value of 'href' attribute) */
+	/* [DONE]get 'href' attribute from the clicked link */
+	const articleSelector = clickedElement.getAttribute("href");
+	console.log(articleSelector);
+	/* [DONE]find the correct article using the selector (value of 'href' attribute) */
+	const targetArticle = document.querySelector(articleSelector);
+	console.log(targetArticle);
 
 	/* add class 'active' to the correct article */
 }
